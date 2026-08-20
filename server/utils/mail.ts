9a814ada23abf260be440450b324c7ad80ec6989
rafaumeu <rafael.zendron22@gmail.com>
2026-08-20 10:30:06 -0300

    fix: remove unused @ts-expect-error in mail.ts
    html-to-text types resolve after Ezequias' fix; the directive now fails typecheck (TS2578)

--- server/utils/mail.ts
+++ server/utils/mail.ts
@@ -1,5 +1,4 @@
 import nodemailer, { type Transporter } from 'nodemailer'
-// @ts-expect-error html-to-text v10 does not ship TypeScript declarations.
 import { convert } from 'html-to-text'
 
 let transporter: Transporter | null = null
